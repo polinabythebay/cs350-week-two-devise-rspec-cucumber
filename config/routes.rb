69f892e5c7dbffda3998a382ae5d382be3704489
Polina Soshnin <psoshnin@wellesley.edu>
2014-02-21 02:10:32 -0500

    set up users routes, show, restrict access

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 
 
+
+
 Rails3DeviseRspecCucumber::Application.routes.draw do
   authenticated :user do
     root :to => 'home#index'
   end
   root :to => "home#index"
   devise_for :users
+  resources :users
 end
